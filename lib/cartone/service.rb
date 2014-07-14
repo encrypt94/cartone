e95e22eb93083a90fe11452609ae8d3480be627a
encrypt <encrypt94@gmail.com>
2014-07-14 21:07:07 +0200

    add abstract service

---
+++ lib/cartone/service.rb
@@ -0,0 +1,19 @@
+module Cartone
+  class Service
+    attr_accessor :base_url, :name, :request
+
+    def initialize(name, base_url, request)
+      self.name = name
+      self.base_url = base_url
+      self.request = request
+    end
+
+    def fetch()
+      raise NoMethodError.new("#{self.name} needs to implement fetch()!")
+    end
+
+    def next_page()
+      raise NoMethodError.new("#{self.name} needs to implement next_page()!")
+    end
+  end
+end
