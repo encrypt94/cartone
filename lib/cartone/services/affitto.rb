31d49248db47710991628b4b3c4a7dfcec8d1670
encrypt <encrypt94@gmail.com>
2014-07-14 22:06:07 +0200

    add first prototype of affito.it service

---
+++ lib/cartone/services/affitto.rb
@@ -0,0 +1,46 @@
+module Cartone
+  module Services
+    class AffittoIt < Service
+      def initialize()
+        super('affitto.it', 'http://www.affitto.it','/elenco.php?comune=Bologna+%28BO%29&comune_ID=&tipologia=appartamento&locali_min=&locali_max=&prezzo_min=&prezzo_max=&mq_min=&mq_max=&codice_tipo_utente=privato&testo=&codice_tipo_annuncio=OI&nazione=IT&id_comune=&SearchButton=CERCA')
+      end
+
+      def fetch()
+        annunci = []
+        page = Nokogiri::HTML(open(self.base_url+self.request))
+        page.css('table.risultati').select do |table|
+          annuncio = Annuncio.new
+          annuncio.service = self.name
+          annuncio.title = table.css('span.colorem').text
+          table.css('a.link_hover_dettaglio').select do |link|
+            annuncio.link = link["href"]
+            if(annuncio.link =~ /id_annuncio=([0-9]*)/)
+              annuncio.id = self.name+$1
+            else
+              next
+            end
+            annuncio.data["price"] = table.css('li.liprezzo').text.gsub(/[^0-9]/,'')
+            annuncio.data["type"] = table.css('ul.dettagli_top > li:nth-child(2)').text
+            annuncio.data["size"] = table.css('ul.dettagli_top > li:nth-child(3)').text.gsub(/[^0-9]/,'')
+          end
+          adv_pg = Nokogiri::HTML(open(annuncio.link))
+          annuncio.description = adv_pg.css("#annuncio_descrizione > #desc_corpo > h2.desc_dettaglio").text
+          adv_pg.css('#anteprime > a > img').select do |img|
+            annuncio.images.push(self.base_url+img["src"])
+          end
+          annunci.push(annuncio)
+        end
+        return annunci
+      end
+
+      def next_page()
+        if self.request =~ /pg=([0-9]*)/
+          page_index = $1.to_i+1
+          self.request.gsub("pg="+$1,"pg="+page_index.to_s)
+        else
+          self.request = self.request+"&pg=2"
+        end
+      end
+    end
+  end
+end
