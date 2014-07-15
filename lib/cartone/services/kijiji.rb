6199b68ef94969e8c1255950dbf7970189b68c80
encrypt <encrypt94@gmail.com>
2014-07-15 23:03:44 +0200

    add first prototype of kijiji.it service

---
+++ lib/cartone/services/kijiji.rb
@@ -0,0 +1,55 @@
+module Cartone
+  module Services
+    class KijijiIt < Service
+      def initialize()
+        super('kijiji.it', 'http://www.kijiji.it', '/case/affitto/annunci-bologna/')
+      end
+
+      def fetch()
+        annunci = []
+        page = Nokogiri::HTML(open(self.base_url+self.request))
+        
+        page.css("#search-results-list > div.search-results-list-item").select do |item|
+          annuncio = Annuncio.new()
+          annuncio.service = self.name
+          annuncio.title = item.css('h2.list-item-title > a').text
+          item.css('h2.list-item-title > a').select do |link|
+            annuncio.id = self.name+link["name"]
+            annuncio.link = link["href"]
+          end
+          annuncio.data["price"] = item.css('.list-item-price').text.gsub(/[^0-9,]/,'')
+          adv_pg = Nokogiri::HTML(open(annuncio.link))
+          annuncio.description = adv_pg.css('#item_description').text.chomp
+          adv_pg.css('#thumb-gallery > a').select do |a|
+            annuncio.images.push(a["href"])
+          end
+          adv_pg.css('#main_attributes > ul > li').select do |li|
+            key = li.css('span.bold').text.chomp.strip.gsub(':','').downcase
+            li.css('span.bold').remove
+            value = li.text.chomp.strip.downcase
+            case key
+            when 'tipo'
+              annuncio.data["type"] = value
+            when 'locali'
+              annuncio.data["locali"] = value.gsub(/[^0-9]/,'')
+            when 'mq'
+              annuncio.data["size"] = value.gsub(/[^0-9]/,'')
+            end
+
+          end
+          annunci.push annuncio
+        end
+        return annunci
+      end
+
+      def next_page()
+        if self.request =~ /\?pg=([0-9]*)/
+          page_index = $1.to_i+1
+          self.request.gsub!("\?pg="+$1, "\?pg="+page_index.to_s)
+        else
+          self.request = self.request+"?pg=2"
+        end
+      end
+    end
+  end
+end
