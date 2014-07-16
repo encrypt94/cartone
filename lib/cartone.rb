3c5a285043cf8342d314cc17b3fad045b94cbec1
encrypt <encrypt94@gmail.com>
2014-07-16 17:09:26 +0200

    add parse_date method to annuncio

--- lib/cartone.rb
+++ lib/cartone.rb
@@ -1,9 +1,9 @@
 require "cartone/version"
 require "nokogiri"
 require "open-uri"
+require "chronic"
 require "cartone/annuncio"
 require "cartone/service"
-
 
 module Cartone
   # Your code goes here...
--- lib/cartone/annuncio.rb
+++ lib/cartone/annuncio.rb
@@ -7,6 +7,24 @@ module Cartone
       self.data = {}
       self.images = []
     end
-    
+
+    def self.parse_date(text)
+      text = text.to_s.downcase
+      text.gsub!(/ieri/,'yesterday ')
+      text.gsub!(/oggi/,'today ')
+      text.gsub!(/gen(naio)?/,'january')
+      text.gsub!(/feb(braio)?/,'february')
+      text.gsub!(/mar(zo)?/,'march')
+      text.gsub!(/apr(ile)?/,'april')
+      text.gsub!(/mag(gio)?/,'may')
+      text.gsub!(/giu(gno)?/,'june')
+      text.gsub!(/lug(lio)?/,'july')
+      text.gsub!(/ago(sto)?/,'august')
+      text.gsub!(/set(tembre)?/,'september')
+      text.gsub!(/ott(obre)?/,'october')  
+      text.gsub!(/nov(embre)?/,'november')
+      text.gsub!(/dic(embre)?/,'december')
+      return Chronic.parse(text)
+    end   
   end
 end
