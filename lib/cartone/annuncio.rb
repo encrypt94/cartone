fd4a8cead9c18114ef1684c3b6e7459e7c45b51d
encrypt <encrypt94@gmail.com>
2014-07-16 17:26:49 +0200

    fix non parsed date cases

--- lib/cartone/annuncio.rb
+++ lib/cartone/annuncio.rb
@@ -10,20 +10,21 @@ module Cartone
 
     def self.parse_date(text)
       text = text.to_s.downcase
+      # Whitespace at the words end fix cases like '14 jun23:42'
       text.gsub!(/ieri/,'yesterday ')
       text.gsub!(/oggi/,'today ')
-      text.gsub!(/gen(naio)?/,'january')
-      text.gsub!(/feb(braio)?/,'february')
-      text.gsub!(/mar(zo)?/,'march')
-      text.gsub!(/apr(ile)?/,'april')
-      text.gsub!(/mag(gio)?/,'may')
-      text.gsub!(/giu(gno)?/,'june')
-      text.gsub!(/lug(lio)?/,'july')
-      text.gsub!(/ago(sto)?/,'august')
-      text.gsub!(/set(tembre)?/,'september')
-      text.gsub!(/ott(obre)?/,'october')  
-      text.gsub!(/nov(embre)?/,'november')
-      text.gsub!(/dic(embre)?/,'december')
+      text.gsub!(/gen(naio)?/,'january ')
+      text.gsub!(/feb(braio)?/,'february ')
+      text.gsub!(/mar(zo)?/,'march ')
+      text.gsub!(/apr(ile)?/,'april ')
+      text.gsub!(/mag(gio)?/,'may ')
+      text.gsub!(/giu(gno)?/,'june ')
+      text.gsub!(/lug(lio)?/,'july ')
+      text.gsub!(/ago(sto)?/,'august ')
+      text.gsub!(/set(tembre)?/,'september ')
+      text.gsub!(/ott(obre)?/,'october ')  
+      text.gsub!(/nov(embre)?/,'november ')
+      text.gsub!(/dic(embre)?/,'december ')
       return Chronic.parse(text)
     end   
   end
