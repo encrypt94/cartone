feed4cc11c3a15da269c03aaecbcd9dcfa0f9e1c
encrypt <encrypt94@gmail.com>
2014-08-18 17:03:35 +0200

    fix elasticsearch query using query_string and show template for annuncio

--- lib/cartone/webapp/webapp.rb
+++ lib/cartone/webapp/webapp.rb
@@ -23,48 +23,50 @@ module Cartone
       annunci = 20
       query =  { 
         query: {
-          multi_match: {
-            query: "",
-            fields: [ 'title', 'description' ],
-            operator: 'or'
+          query_string: {
+            default_field: "_all",
+            query: ""
           }
         },
-        post_filter: {
-          range: {}
-        },
-        sort: [ 
-               { 
-                 "data.price" =>
-                 {
-                   order: "asc"
+        sort: [
+               {
+                 "data.price" => {
+                   missing: "_last"
                  }
-               } 
+               },
+               { 
+                 "data.camere" => {
+                   missing: "_last"
+                 }
+               },
+               {
+                 "data.locali" => {
+                   missing: "_last"
+                 }
+               },
+               {
+                 "data.size" => {
+                   missing: "_last"
+                 }
+               }
               ]
       }
+      query[:query][:query_string][:query] = "((_missing_:data.price) OR (data.price:[#{params[:price_from].to_f} TO #{params[:price_to].to_f}])) AND ((_missing_:data.locali) OR (data.locali:[#{params[:locali_from].to_i} TO #{params[:locali_to].to_i}])) AND ((_missing_:data.camere) OR (data.camere:[#{params[:camere_from].to_i} TO #{params[:camere_to].to_i}])) AND ((_missing_:data.size) OR (data.size:[#{params[:size_from].to_i} TO #{params[:size_to].to_i}]))"
       if params.has_key?('str') and !params[:str].empty?
-        query[:query][:multi_match][:query] = params[:str]
-      else
-        query.delete(:query)
-      end
-      pars.each do |par|
-        query[:post_filter][:range][par] = {}
-        ['from','to'].each do |m|
-          if params.has_key?(par+"_"+m)
-            query[:post_filter][:range][par][m] = params[par+"_"+m].to_f
-          else
-            query[:post_filter][:range][par][m] = 0
-          end
-        end
+        #Sanitize input
+        params[:str].gsub!('"','\"')
+        query[:query][:query_string][:query] << " AND (title:(\"#{params[:str]}\") OR description:(\"#{params[:str]}\"))"
       end
       results = es.search(index: 'annunci', type: 'annuncio', body: query, size: annunci, from: page.to_i*annunci)
       erb :annunci, locals: { query_string: request.query_string, annunci: results['hits']['hits'], results: results['total'], pages: results['hits']['total'].to_i/annunci, current: page.to_i }
     end
 
     get '/annuncio/:id' do |id|
-      content_type :json
       if es.exists(index: 'annunci', type: 'annuncio', id: id)
-        annuncio = es.get(index: 'annunci', type: 'annuncio', id: id)
-        annuncio.to_json
+        result = es.get(index: 'annunci', type: 'annuncio', id: id)
+        erb :annuncio, locals: { annuncio: result['_source']}
+      else
+        404
       end
     end
   end
