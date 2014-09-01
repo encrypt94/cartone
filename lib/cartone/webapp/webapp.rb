4df1862f0f4544e828321a954467417e92decf21
encrypt <encrypt94@gmail.com>
2014-09-01 11:22:51 +0200

    fix server bind

--- lib/cartone/webapp/webapp.rb
+++ lib/cartone/webapp/webapp.rb
@@ -1,5 +1,6 @@
 module Cartone
   class Webapp < Sinatra::Base
+    set :bind, '0.0.0.0' 
     set :views, File.expand_path('../views', __FILE__)
     set :public_folder, File.expand_path('../public', __FILE__)
     es = Elasticsearch::Client.new(log: true)
